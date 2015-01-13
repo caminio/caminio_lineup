36fb2e863645c876dcd3623c5f18faf254fe8dbf
David Reinisch <david.reinisch@gmx.at>
2015-01-13 16:05:35 +0100

    venue

---
+++ lib/caminio_lineup/lineup_attributes.rb
@@ -0,0 +1,23 @@
+module LineupAttributes
+
+  extend ActiveSupport::Concern
+
+  included do
+
+    field :tags, type: Array, default: []
+
+    field :ext_ref_id, type: String
+    field :ext_ref_src, type: String
+    field :ext_ref_note, type: String
+    field :ext_ref_sync_at, type: String
+
+    field :orig_url, type: String
+    field :ext_url, type: String
+    field :video_url, type: String
+    field :video_type, type: String
+
+    field :others_write, type: String
+    field :notify_me_on_write, type: Boolean, default: true
+
+  end
+end
