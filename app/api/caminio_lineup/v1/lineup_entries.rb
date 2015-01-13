2d86538fc82a27644acedc4fa9820eb9aa445c5d
David Reinisch <david.reinisch@gmx.at>
2015-01-13 13:56:01 +0100

    basic CRUD for lineup entries

--- app/api/caminio_lineup/v1/lineup_entries.rb
+++ app/api/caminio_lineup/v1/lineup_entries.rb
@@ -27,6 +27,50 @@ module Caminio
         present :lineup_entry, entry, with: LineupEntryEntity
       end
 
+      #
+      # POST /
+      #
+      desc "create a new lineup_entry"
+      params do
+        requires :lineup_entry, type: Hash do
+          optional :categories
+        end
+      end
+      post do
+        authenticate!
+        entry = LineupEntry.new( declared( params )[:lineup_entry] )
+        error!({ error: 'SavingFailed', details: entry.errors.full_messages}, 422) unless entry.save
+        present :lineup_entry, entry, with: LineupEntryEntity
+      end
+
+      #
+      # PUT /:id
+      #
+      desc "update an existing lineup_entry"
+      params do
+        requires :lineup_entry, type: Hash do
+          optional :categories
+        end
+      end
+      put '/:id' do
+        authenticate!
+        entry = LineupEntry.find params.id 
+        error! "LineupEntryNotFound", 404 unless entry
+        entry.update_attributes( declared(params)[:lineup_entry] )
+        present :lineup_entry, entry.reload, with: LineupEntryEntity
+      end
+
+      #
+      # DELETE /:id
+      #
+      desc "delete an lineup_entry"
+      formatter :json, lambda{ |o,env| "{}" }
+      delete '/:id' do
+        authenticate!
+        entry = LineupEntry.find params.id 
+        error! "LineupEntryNotFound", 404 unless entry
+        error!("DeletionFailed",500) unless entry.destroy
+      end
 
     end
   end
--- spec/api/lineup_entries_api_spec.rb
+++ spec/api/lineup_entries_api_spec.rb
@@ -15,7 +15,6 @@ describe Caminio::V1::LineupEntries do
     it "returns lineup_entries json" do
       get @url
       expect( last_response.status ).to be == 200
-      puts last_response.body.inspect
       expect( json ).to have_key(:lineup_entries)
     end
 
@@ -25,18 +24,88 @@ describe Caminio::V1::LineupEntries do
         get "v1/lineup_entries/#{@lineup_entry.id}"
       end
 
-      it{ puts json.lineup_jobs.inspect }
       it{ expect( json.lineup_entry ).to have_key(:id) }
       it{ expect( json.lineup_entry ).to have_key(:categories) }
-    #   it{ expect( json.user ).to have_key(:firstname) }
-    #   it{ expect( json.user ).to have_key(:lastname) }
-    #   it{ expect( json.user ).to have_key(:email) }
-    #   it{ expect( json.user ).to have_key(:admin) }
-    #   it{ expect( json.user ).to have_key(:editor) }
-    #   it{ expect( json.user ).not_to have_key(:password_digest) }
 
     end
 
   end
+  describe "POST /lineup_entries" do
+
+    before :each do
+      @user = create(:user)
+      header 'Authorization', "Bearer #{@user.aquire_api_key.token}"
+    end
+
+    let(:url){ 'v1/lineup_entries' }
+    let(:error_400){ 'lineup_entry is missing' }
+    let(:post_attr){ { lineup_entry: { categories: ["two", "three"] } } }
+
+    describe "requires" do
+
+      it { post(url); expect( last_response.status ).to be == 400 }
+
+      it { post(url); expect( json.error ).to be == error_400 }
+
+      it { post(url, { lineup_entry: { } } ); expect( json.error ).to be == error_400 }
+
+    end
+
+    describe "returns lineup_entry" do
+      
+      before :each do
+        post url, post_attr 
+      end
+
+      it{ expect( last_response.status ).to be == 201 }
+
+      it{ expect( json ).to have_key :lineup_entry }
+
+    end
+
+  end
+
+  describe "PUT /lineup_entries/:id" do
+
+    before :each do
+      @user = create(:user)
+      @lineup_entry = create(:lineup_entry)
+      header 'Authorization', "Bearer #{@user.aquire_api_key.token}"
+      header 'Organization-id', @user.organizations.first
+    end
+
+    describe "update" do
+
+      describe "categories" do
+
+        before :each do
+          @new_categories = ["new", "categories"]
+          put "v1/lineup_entries/#{@lineup_entry.id}", { lineup_entry: { categories: @new_categories } }
+        end
+
+        it { expect( last_response.status ).to eq(200) }
+        it { expect( json.lineup_entry.categories ).to eq( @new_categories ) }
+
+      end
+
+    end
+
+  end
+
+  describe "DELETE /lineup_entries/:id" do
+
+    before :each do
+      @user = create(:user)
+      @lineup_entry = create(:lineup_entry)
+      header 'Authorization', "Bearer #{@user.aquire_api_key.token}"
+      header 'Organization-id', @user.organizations.first
+      delete "v1/lineup_entries/#{@lineup_entry.id}"
+    end
+
+    it{ expect( last_response.status ).to be == 200 }
+    it{ expect( json ).to eq({}) }
+
+  end
+
 end
 
