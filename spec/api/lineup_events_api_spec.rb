689baa73064bf291a8855148e9429e41306f9ad0
David Reinisch <david.reinisch@gmx.at>
2015-02-05 13:34:30 +0100

    passing

--- spec/api/lineup_events_api_spec.rb
+++ spec/api/lineup_events_api_spec.rb
@@ -2,41 +2,6 @@ require 'spec_helper'
 
 describe Caminio::V1::LineupEvents do
 
-  # describe "/lineup_events" do
-
-  #   before :each do
-  #     @user = create(:user)
-  #     @lineup_entry = create(:lineup_entry)
-  #     # @lineup_event = create(:lineup_event, lineup_entry: @lineup_entry )
-  #     @lineup_event = @lineup_entry.events.create(starts: Date.new())
-  #     puts @lineup_event.inspect
-  #     puts @lineup_entry.inspect
-  #     puts @lineup_entry.events.inspect
-  #     puts "========================"
-  #     @url = "v1/lineup_events"
-  #     header 'Authorization', "Bearer #{@user.aquire_api_key.token}"
-  #     header 'Organization-id', @user.organizations.first
-  #   end
-
-  #   it "returns lineup_events json" do
-  #     get @url
-  #     expect( last_response.status ).to be == 200
-  #     expect( json ).to have_key(:lineup_events)
-  #   end
-
-  #   describe "json return properties" do
-
-  #     before :each do
-  #       get "v1/lineup_events/#{@lineup_event.id}"
-  #     end
-  #     it{ puts json.inspect }
-  #     it{ expect( json.lineup_event ).to have_key(:id) }
-  #     it{ expect( json.lineup_event ).to have_key(:starts) }
-
-  #   end
-
-  # end
-
   describe "POST /lineup_events", focus: true do
 
     before :each do
@@ -51,7 +16,7 @@ describe Caminio::V1::LineupEvents do
     let(:error_400){ 'lineup_event is missing' }
     let(:post_attr){ { 
       lineup_event: { 
-        starts: Date.new(), 
+        starts: DateTime.new(), 
         lineup_venue_id: @lineup_venue.id, 
         lineup_ensemble_ids: [ @lineup_ensemble.id ] }, 
       lineup_entry_id: @lineup_entry.id } 
@@ -90,7 +55,7 @@ describe Caminio::V1::LineupEvents do
     before :each do
       @user = create(:user)
       @lineup_entry = create(:lineup_entry)
-      @lineup_event = @lineup_entry.events.create(starts: Date.new())      
+      @lineup_event = @lineup_entry.events.create(starts: DateTime.new())      
       header 'Authorization', "Bearer #{@user.aquire_api_key.token}"
       header 'Organization-id', @user.organizations.first
     end
@@ -100,7 +65,7 @@ describe Caminio::V1::LineupEvents do
       describe "title" do
 
         before :each do
-          @new_starts = Date.new() 
+          @new_starts = DateTime.new() 
           put "v1/lineup_events/#{@lineup_event.id}", { lineup_event: { starts: @new_starts }, lineup_entry_id: @lineup_entry.id  }
         end
 
