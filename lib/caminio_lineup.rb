08522b991c932b4c41226fdf4017f9637ed428ce
thorsten zerha <thorsten.zerha@tastenwerk.com>
2015-02-10 13:38:51 +0100

    up

--- lib/caminio_lineup.rb
+++ lib/caminio_lineup.rb
@@ -1,4 +1,11 @@
-# 3rd
 require "caminio_lineup/version"
+
+# workaround for the following LineupEvent senders
+# to keep compatibility with development mode when starting
+# a rails server
+if Rails.env == 'development'
+  require File::join File::dirname(__FILE__), '../../caminio/app/models/location'
+end
+
 require "caminio_lineup/engine"
 require "caminio_lineup/lineup_attributes"
