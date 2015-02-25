2843a5689a5e23e0f90fd86e414ffd3c3dad9862
David Reinisch <david.reinisch@gmx.at>
2015-02-25 16:44:07 +0100

    venue destroys event

--- app/models/lineup_venue.rb
+++ app/models/lineup_venue.rb
@@ -6,5 +6,16 @@ class LineupVenue < Location
   field :reach_by_tram, type: String
   field :reach_by_train, type: String
   field :opening_hours, type: String
+
+  before_destroy :destroy_events
+
+  private
+
+    def destroy_events
+      LineupEntry.elem_match( events: { lineup_venue_id: self.id } ).each do |entry|
+        entry.events.each { |event| event.destroy if event.lineup_venue_id == self.id }
+      end
+    end 
+
   
 end
